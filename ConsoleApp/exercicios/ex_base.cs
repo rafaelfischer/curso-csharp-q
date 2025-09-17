456af31d5623b263d23ac2759d51bfde2b5cd3e5
Rafael Fischer <rafaelfischer@gmail.com>
2025-09-17 15:42:23 -0300

    Alteração do código do arquivo base

---
+++ ConsoleApp/exercicios/ex_base.cs
@@ -0,0 +1,19 @@
+namespace ConsoleApp.exercicios {
+    public class ex_base {
+        public static void Main(string[] args) {
+            //tutorial: https://www.tutorialspoint.com/cprogramming/index.htm
+            //C online compiler: https://www.onlinegdb.com/online_c_compiler
+
+
+            //declaring a variable
+            //int number;
+            //number = 10; //assignment
+            //int number = 10; //declaration + assignment
+            //reading a variable from keyboard
+            //int number = int.Parse(Console.ReadLine());
+
+            //printing a variable
+            //Console.WriteLine("The number is {0}", number);
+        }
+    }
+}
